51446f0008b6bc462407bd1ad6dc2b5014a9c466
Temirlan Nurmatov <borukcuevt@gmail.com>
2024-06-18 19:57:13 +0600

    save

---
+++ src/page/AboutCompany.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AboutCompany = () => {
+  return <div>AboutCompany</div>;
+};
+
+export default AboutCompany;
---
+++ src/page/Сooperation.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Сooperation = () => {
+  return <div>Сooperation</div>;
+};
+
+export default Сooperation;
